d1650075bfd244b2cfbc347263f197494010e08b
wikuuu1 <wiktoria.rajba391@gmail.com>
2021-10-19 20:30:16 +0200

    implemented proximity_of_ships() and get_direction_for_size_two_ship(), modified mark()

--- battleship.py
+++ battleship.py
@@ -4,7 +4,7 @@ import os
 SIZE = 5
 EMPTY_SPACE = "O"
 SHIP_PLACED = "X"
-
+SHIPS_TO_PLACE = 6
 
 # clears the screen
 def console_clear():
@@ -35,13 +35,13 @@ def print_board():
 
 
 # validates user's input's format
-def validate_format(user_input): 
-    # Change to range(2,4) for custom board size 
+def validate_format(user_input):
+    # Change to range(2,4) for custom board size
     if len(user_input) in range(2,3) and user_input[1].isdigit() and user_input[0].isalpha():
         return True
     else:
         return False
-   
+
     # second_index = int(user_move[1:])
     # print(second_index)
 
@@ -51,7 +51,7 @@ def proximity_of_ships(row, col):
         return True
     if BOARD[row][col - 1] == SHIP_PLACED:
         return True
-    if col + 1 < SIZE:    
+    if col + 1 < SIZE:
         if BOARD[row][col + 1] == SHIP_PLACED:
             return True
     if row + 1 < SIZE:
@@ -59,9 +59,32 @@ def proximity_of_ships(row, col):
             return True
     return False
 
+def get_direction_for_size_two_ship(row, col, direction):
+    while True:
+        direction = input("Please choose direction(up/down/left/right): ")
+
+        if direction == "up":
+            print(direction)
+            if not proximity_of_ships(row - 1, col):
+                return row - 1, col, direction
+        if direction == "down":
+            if not proximity_of_ships(row + 1, col):
+                return row + 1, col, direction
+        if direction == "right":
+            if not proximity_of_ships(row, col + 1):
+                return row, col + 1, direction
+        if direction == "left":
+            if not proximity_of_ships(row, col - 1):
+                return row, col - 1, direction
+        else:
+            print("INVALID INPUT")
+            continue
+
 # takes user input and checks its validity
 def get_move():
+
     while True:
+        direction = None
         user_move = input("Please give your coordinates: ")
         if validate_format(user_move):
             row = ord(user_move[0].lower()) - 97
@@ -73,22 +96,43 @@ def get_move():
             if proximity_of_ships(row, col):
                 print("Ships are too close!")
                 continue
+            if SHIPS_TO_PLACE > 3:
+                return get_direction_for_size_two_ship(row, col, direction)
+
             else:
-                return row, col
+                return row, col, direction
         else:
             print("")
 
 # marks user's choice of placement of the ships and returns marked board
 def mark():
     global BOARD
-    row, col = get_move()
-    BOARD[row][col] = SHIP_PLACED
+    row, col, direction = get_move()
+    if direction == None:
+        BOARD[row][col] = SHIP_PLACED
+    if direction == "up":
+        BOARD[row][col] = SHIP_PLACED
+        BOARD[row + 1][col] = SHIP_PLACED
+    if direction == "down":
+        BOARD[row][col] = SHIP_PLACED
+        BOARD[row - 1][col] = SHIP_PLACED
+    if direction == "right":
+        BOARD[row][col] = SHIP_PLACED
+        BOARD[row][col + 1] = SHIP_PLACED
+    if direction == "left":
+        BOARD[row][col] = SHIP_PLACED
+        BOARD[row][col - 1] = SHIP_PLACED
     print_board()
 
 
+
 def main():
-    while True:
-        mark()                
+    print_board()
+    global SHIPS_TO_PLACE
+    while SHIPS_TO_PLACE > 0:
+        mark()
+        SHIPS_TO_PLACE -= 1
+        print(SHIPS_TO_PLACE)
 
 if __name__ == "__main__":
     main()
