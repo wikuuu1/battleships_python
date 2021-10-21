b810d9cb585e62d18ea166ead8a5cf9df219e96a
Chomentowski Sebastian <chomentowski.s@gmail.com>
2021-10-21 18:23:16 +0200

    implemented play(), place_ships(), changed ships_too_close and main()

--- battleship.py
+++ battleship.py
@@ -49,16 +49,16 @@ def validate_format(user_input):
     # print(second_index)
 
 # checks if ships aren't too close
-def proximity_of_ships(row, col):
-    if BOARD[row - 1][col] == SHIP_PLACED and not row == 0:
+def ships_too_close(board, row, col):
+    if board[row - 1][col] == SHIP_PLACED and not row == 0:
         return True
-    if BOARD[row][col - 1] == SHIP_PLACED and not col == 0:
+    if board[row][col - 1] == SHIP_PLACED and not col == 0:
         return True
     if col + 1 < SIZE:
-        if BOARD[row][col + 1] == SHIP_PLACED and not col == 4:
+        if board[row][col + 1] == SHIP_PLACED and not col == 4:
             return True
     if row + 1 < SIZE:
-        if BOARD[row + 1][col] == SHIP_PLACED and not row == 4:
+        if board[row + 1][col] == SHIP_PLACED and not row == 4:
             return True
     return False
 
@@ -66,16 +66,16 @@ def get_direction_for_size_two_ship(row, col, direction):
     while True:
         direction = input("Please choose direction(up/down/left/right): ")
         if direction == "up" and not row == 0:
-            if not proximity_of_ships(row - 1, col):
+            if not ships_too_close(BOARD, row - 1, col):
                 return row - 1, col, direction
         if direction == "down" and not row == 4:
-            if not proximity_of_ships(row + 1, col):
+            if not ships_too_close(BOARD, row + 1, col):
                 return row + 1, col, direction
         if direction == "right" and not col == 4:
-            if not proximity_of_ships(row, col + 1):
+            if not ships_too_close(BOARD, row, col + 1):
                 return row, col + 1, direction
         if direction == "left" and not col == 0:
-            if not proximity_of_ships(row, col - 1):
+            if not ships_too_close(BOARD, row, col - 1):
                 return row, col - 1, direction
         else:
             print("INVALID INPUT")
@@ -94,9 +94,9 @@ def get_move():
             if not row <= lenght_of_board and not col <= lenght_of_board:
                 print("Invalid input")
                 continue
-            if SHIPS_TO_PLACE > 3 and not proximity_of_ships(row, col):
+            if SHIPS_TO_PLACE > 3 and not ships_too_close(BOARD, row, col):
                 return get_direction_for_size_two_ship(row, col, direction)
-            if proximity_of_ships(row, col):
+            if ships_too_close(BOARD, row, col):
                 print("Ships are too close!")
                 continue
             else:
@@ -146,11 +146,71 @@ def print_two_boards(empty_board_one, empty_board_two):
         print()
         print()
 
+def play(empty_board_one, empty_board_two, player_one_board, player_two_board):
+    while True:
+        print_two_boards(empty_board_one, empty_board_two)
+        user_input = input("Please give your coordinates: ")
+        if validate_format(user_input):
+            row = ord(user_input [0].lower()) - 97
+            col = int(user_input [1]) - 1
+            lenght_of_board = len(BOARD)
+            if not row <= lenght_of_board and not col <= lenght_of_board:
+                print("Invalid input")
+                continue
+        if player_two_board[row][col] == EMPTY_SPACE:
+            empty_board_two[row][col] = "M"
+        if player_two_board[row][col] == SHIP_PLACED:
+            if ships_too_close(player_two_board, row, col):
+                empty_board_two[row][col] = "H"
+                player_two_board[row][col] = "H"
+            else:
+                empty_board_two[row][col] = "S"
+                if empty_board_two[row - 1][col] == "H" and not row == 0:
+                    empty_board_two[row - 1][col] = "S"
+                if empty_board_two[row][col - 1] == "H" and not col == 0:
+                    empty_board_two[row][col - 1] = "S"
+                if col + 1 < SIZE:
+                    if empty_board_two[row][col + 1] == "H" and not col == 4:
+                        empty_board_two[row][col + 1] = "S"
+                if row + 1 < SIZE:
+                    if empty_board_two[row + 1][col] == "H" and not row == 4:
+                        empty_board_two[row + 1][col] = "S"
 
-def main():
+        print_two_boards(empty_board_one, empty_board_two)
+
+        user_input = input("Please give your coordinates: ")
+        if validate_format(user_input):
+            row = ord(user_input [0].lower()) - 97
+            col = int(user_input [1]) - 1
+            lenght_of_board = len(BOARD)
+            if not row <= lenght_of_board and not col <= lenght_of_board:
+                print("Invalid input")
+                continue
+        if player_one_board[row][col] == EMPTY_SPACE:
+            empty_board_one[row][col] = "M"
+        if player_one_board[row][col] == SHIP_PLACED:
+            if ships_too_close(player_one_board, row, col):
+                empty_board_one[row][col] = "H"
+                player_one_board[row][col] = "H"
+            else:
+                empty_board_one[row][col] = "S"
+                if empty_board_one[row - 1][col] == "H" and not row == 0:
+                    empty_board_one[row - 1][col] = "S"
+                if empty_board_one[row][col - 1] == "H" and not col == 0:
+                    empty_board_one[row][col - 1] = "S"
+                if col + 1 < SIZE:
+                    if empty_board_one[row][col + 1] == "H" and not col == 4:
+                        empty_board_one[row][col + 1] = "S"
+                if row + 1 < SIZE:
+                    if empty_board_one[row + 1][col] == "H" and not row == 4:
+                        empty_board_one[row + 1][col] = "S"
+
+        print_two_boards(empty_board_one, empty_board_two)
+
+def place_ships():
     global BOARD
-    print_board()
     global SHIPS_TO_PLACE
+    print_board()
     while SHIPS_TO_PLACE > 0:
         mark()
         SHIPS_TO_PLACE -= 1
@@ -167,13 +227,14 @@ def main():
         print(SHIPS_TO_PLACE)
     player_two_board = BOARD
 
+    empty_board_one  = generate_board()
+    empty_board_two = generate_board()
 
-    BOARD = generate_board()
-    empty_board_one  = BOARD
-    empty_board_two = BOARD
+    play(empty_board_one, empty_board_two, player_one_board, player_two_board)
 
-    print_two_boards(empty_board_one, empty_board_two)
+def main():
+    place_ships()
+
 
 if __name__ == "__main__":
     main()
-    print_two_boards()
